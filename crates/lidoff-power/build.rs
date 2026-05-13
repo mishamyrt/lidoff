32a896f50516eacfb73ad60a9f4b1e3856c36759
mishamyrt <misha@myrt.co>
2026-05-13 14:45:41 +0300

    chore: set correct targets for power crate

--- crates/lidoff-power/build.rs
+++ crates/lidoff-power/build.rs
@@ -1,6 +1,8 @@
 fn main() {
-    println!("cargo:rerun-if-changed=src/power.c");
-    println!("cargo:rerun-if-changed=src/power.h");
+    println!("cargo:rerun-if-changed=src/power_observer.c");
+    println!("cargo:rerun-if-changed=src/power_observer.h");
+    println!("cargo:rerun-if-changed=src/caffeinate.c");
+    println!("cargo:rerun-if-changed=src/caffeinate.h");
 
     cc::Build::new()
         .file("src/power_observer.c")
